5e593f7401a8b8dd83144ba6ba15fcf3fce7ef74
Spencer Couture <spence.couture@gmail.com>
2024-06-24 21:36:48 -0400

    Fixed PS1 variable
    this was broken before so i copied what worked on my mac

--- .bash_scripts.bash
+++ .bash_scripts.bash
@@ -1,4 +1,11 @@
-export PS1="\e[1;31;40m\$\e[m " 
+#export PS1="\e[1;31;40m\$\e[m " 
+
+force_color_prompt=yes
+# export PS1="\[$(tput setaf 33)\]\u\[$(tput setaf 69)\]:\[$(tput setaf 141)\]\w \[$(tput sgr0)\]$ "
+PROMPT_COLOR="\[\e[1;31;40m\]"
+COLOR_RESET="\[\e[00m\]"
+# export PS1="\[\e[1;31;40m\]\$\[\e[00m\] "
+export PS1="${PROMPT_COLOR}\$${COLOR_RESET} "
 
 if [ -f ~/.aliases ]; then
   source ~/.aliases
