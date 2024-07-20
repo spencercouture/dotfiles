841705706287dcd7e91088020d26b9c742b88dc9
Spencer Couture <spence.couture@gmail.com>
2024-07-20 17:40:03 -0400

    Added .vimrc, installation details in README
    Also added `export EDITOR=vim` to the bash_scripts.bash

--- .bash_scripts.bash
+++ .bash_scripts.bash
@@ -1,6 +1,8 @@
 #export PS1="\e[1;31;40m\$\e[m " 
 
+# unknown if this is necessary... apparently allows/forces(?) colors in the bash prompt. odd.
 force_color_prompt=yes
+
 # export PS1="\[$(tput setaf 33)\]\u\[$(tput setaf 69)\]:\[$(tput setaf 141)\]\w \[$(tput sgr0)\]$ "
 PROMPT_COLOR="\[\e[1;31;40m\]"
 COLOR_RESET="\[\e[00m\]"
@@ -10,3 +12,6 @@ export PS1="${PROMPT_COLOR}\$${COLOR_RESET} "
 if [ -f ~/.aliases ]; then
   source ~/.aliases
 fi
+
+# set our EDITOR variable to vim
+export EDITOR=vim
---
+++ .vimrc
@@ -0,0 +1,1 @@
+autocmd FileType gitcommit call setpos('.', [0, 1, 1, 0])
